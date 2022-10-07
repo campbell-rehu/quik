79ccfcf2d350267fbcb365fa9a78fc2979d4c8f1
Campbell <campbellrehu@me.com>
2022-10-07 14:21:45 +1300

    quick -> quik

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -5,7 +5,7 @@ import { Page } from './types'
 export const Splash: React.FC<{}> = () => {
   return (
     <div>
-      <h1>quick</h1>
+      <h1>quik</h1>
       <Button to={Page.Lobby} />
     </div>
   )
