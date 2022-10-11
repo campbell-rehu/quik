7fa05fc6811a5cf755763cdede123ff752cb1c64
Campbell <campbellrehu@me.com>
2022-10-11 22:01:25 +1300

    Add apk add jq to script

--- quik-fe-docker-entrypoint.sh
+++ quik-fe-docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
+apk add --no-cache jq
+
 # Capture all environment variables starting with REACT_APP_ and make JSON string from them
 ENV_JSON="$(jq --compact-output --null-input 'env | with_entries(select(.key | startswith("REACT_APP_")))')"
 
