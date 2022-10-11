b37c0832e02d274b968b4543abd8c04d9347a861
Campbell <campbellrehu@me.com>
2022-10-12 06:27:24 +1300

    handle undefined

--- src/pages/config.ts
+++ src/pages/config.ts
@@ -1,2 +1,2 @@
 export const SERVER_URL =
-  window.ENV.REACT_APP_API_URL || process.env.REACT_APP_SERVER_URL || ''
+  window.ENV?.REACT_APP_API_URL || process.env.REACT_APP_SERVER_URL || ''
--- src/pages/globals.ts
+++ src/pages/globals.ts
@@ -3,8 +3,8 @@ export {}
 // Setting this global type override to allow setting environment variables at runtime via the Docker image
 declare global {
   interface Window {
-    ENV: {
-      REACT_APP_API_URL: string
+    ENV?: {
+      REACT_APP_API_URL?: string
     }
   }
 }
